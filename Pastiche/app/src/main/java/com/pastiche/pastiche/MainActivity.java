1f56689af6e5f00cf8f2d707e71f1729f032ba12
ariapahlavan <apahlavan1@utexas.edu>
2016-10-14 18:55:22 -0500

    change v21/activity_main to be a LinearActivity

--- Pastiche/app/src/main/java/com/pastiche/pastiche/MainActivity.java
+++ Pastiche/app/src/main/java/com/pastiche/pastiche/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
     }
 }
