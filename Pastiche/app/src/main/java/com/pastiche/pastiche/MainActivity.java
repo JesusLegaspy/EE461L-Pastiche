7ef7b7622780585ac2432236270b91c0a20eeebe
ariapahlavan <apahlavan1@utexas.edu>
2016-10-15 17:37:51 -0500

    adding menu items to app bar

--- Pastiche/app/src/main/java/com/pastiche/pastiche/MainActivity.java
+++ Pastiche/app/src/main/java/com/pastiche/pastiche/MainActivity.java
@@ -1,10 +1,15 @@
 package com.pastiche.pastiche;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar main_toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+        main_toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(main_toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate the option menu
+        
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
