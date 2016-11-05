6ba690c1d1e61d55e19b1c37749dc11f72878264
Jesus Legaspy <jesus.legaspy@utexas.edu>
2016-11-05 12:24:34 -0500

    Cleaned up code.

--- Pastiche/app/src/main/java/com/pastiche/pastiche/MainActivity.java
+++ Pastiche/app/src/main/java/com/pastiche/pastiche/MainActivity.java
@@ -2,31 +2,17 @@ package com.pastiche.pastiche;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.NetworkResponse;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.net.CookieHandler;
 import java.net.CookieManager;
@@ -84,132 +70,14 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 //-------------------[TESTING]------------------------
-    public void testLogin(View view) throws JSONException {
+    public void testLogin(View view) {
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            //loginTest();
-            //imageTest();
-            layeredPost();
+            //do work
         } else {
-            // display error
-            Context context = getApplicationContext();
-            CharSequence text = "No internet connection!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
+            //log
         }
-    }
-
-    TextView mTxtDisplay;
-    private void loginTest () throws JSONException {
-        mTxtDisplay = (TextView) findViewById(R.id.responseText);
-
-        JSONObject body = new JSONObject();
-        body.put("username","myname");
-        body.put("password","mypassword");
-        body.put("email","myemail@no.co");
-
-        Response.Listener<JSONObject> successful = new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        mTxtDisplay.setText("Response: " + response.toString());
-
-                    }
-
-                };
-
-        Response.ErrorListener errored = new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
-                        mTxtDisplay.setText("Response: " + error.toString());
-                    }
-                };
-
-        //ServerRequestHandler.createAccountRequest(this.getApplicationContext(), body, successful, errored);
-    }
-    ImageView mImageView;
-    private void imageTest(){
-        String url = "http://api.pastiche.staging.jacobingalls.rocks/photos/14";
-        mImageView = (ImageView) findViewById(R.id.imageView);
-
-// Retrieves an image specified by the URL, displays it in the UI.
-        ImageRequest request = new ImageRequest(url,
-                new Response.Listener<Bitmap>() {
-                    @Override
-                    public void onResponse(Bitmap bitmap) {
-                        mImageView.setImageBitmap(bitmap);
-                    }
-                }, 0, 0, null,
-                new Response.ErrorListener() {
-                    public void onErrorResponse(VolleyError error) {
-                        mImageView.setImageResource(R.drawable.magnify);
-                    }
-                });
-// Access the RequestQueue through your singleton class.
-        // Access the RequestQueue through your singleton class.
-        ServerRequestQueue test = ServerRequestQueue.getInstance(this.getApplicationContext());
-        test.addToRequestQueue(request);
-    }
-
-    public void layeredPost() throws JSONException {
-        mTxtDisplay = (TextView) findViewById(R.id.responseText);
-        ServerRequestHandler testing = ServerRequestHandler.getInstance(this.getApplicationContext());
-        JSONObject body = new JSONObject();
-        body.put("username","myname");
-        body.put("password","mypassword");
-        body.put("email","myemail@no.co");
-
-        testing.jsonPost("/users", body, x -> {
-                    Log.d("main",x.toString());
-            mTxtDisplay.setText("Response: " + x.toString());},
-                y -> {
-            if(y != null) {
-                onErrorResponse(y);
-            }
-        });
-        //mTxtDisplay.setText("Response: " + y.toString())
-    }
-
-    //Credit from "Submersed" on https://stackoverflow.com/questions/21867929/android-how-handle-message-error-from-the-server-using-volley
-
-    public void onErrorResponse(VolleyError error) {
-        String json = null;
-
-        NetworkResponse response = error.networkResponse;
-        if(response != null && response.data != null){
-            switch(response.statusCode){
-                case 400:
-                    json = new String(response.data);
-                    json = trimMessage(json, "error");
-                    if(json != null) displayMessage(json);
-                    break;
-            }
-            //Additional cases
-        }
-    }
-    public String trimMessage(String json, String key){
-        String trimmedString = null;
-
-        try{
-            JSONObject obj = new JSONObject(json);
-            trimmedString = obj.getString(key);
-        } catch(JSONException e){
-            e.printStackTrace();
-            return null;
-        }
-
-        return trimmedString;
-    }
-
-    //Somewhere that has access to a context
-    public void displayMessage(String toastString){
-        //Toast.makeText(context, toastString, Toast.LENGTH_LONG).show();
-        Log.d("main", toastString);
     }
 }
--- Pastiche/app/src/main/java/com/pastiche/pastiche/ServerHandler.java
+++ Pastiche/app/src/main/java/com/pastiche/pastiche/ServerHandler.java
@@ -1,18 +1,24 @@
 package com.pastiche.pastiche;
 
-import com.android.volley.Request;
-import com.google.gson.Gson;
+        import android.util.Log;
+
+        import com.android.volley.NetworkResponse;
+        import com.android.volley.VolleyError;
+
+        import org.json.JSONException;
+        import org.json.JSONObject;
 
 /**
  * Created by Khalid on 10/22/2016.
  */
-/*
+
 public class ServerHandler {
     public PUser createAccountRequest(){
         //check if username is in use
         return null;
     }
 
+    /*
     public void loginRequest(String usern, String passw, ResponseUI respUI) {
         Gson gson = new Gson();
         String tmp = gson.toJson(new login(usern, passw));
@@ -48,6 +54,42 @@ class login{
     public login(String usern, String passw){
         this.username = usern;
         this.password = passw;
+    }*/
+
+//Credit from "Submersed" on https://stackoverflow.com/questions/21867929/android-how-handle-message-error-from-the-server-using-volley
+
+    public void onErrorResponse(VolleyError error) {
+        String json = null;
+
+        NetworkResponse response = error.networkResponse;
+        if(response != null && response.data != null){
+            switch(response.statusCode){
+                case 400:
+                    json = new String(response.data);
+                    json = trimMessage(json, "error");
+                    if(json != null) displayMessage(json);
+                    break;
+            }
+            //Additional cases
+        }
+    }
+    public String trimMessage(String json, String key){
+        String trimmedString = null;
+
+        try{
+            JSONObject obj = new JSONObject(json);
+            trimmedString = obj.getString(key);
+        } catch(JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+
+        return trimmedString;
+    }
+
+    //Somewhere that has access to a context
+    public void displayMessage(String toastString){
+        //Toast.makeText(context, toastString, Toast.LENGTH_LONG).show();
+        Log.d("main", toastString);
     }
 }
-*/
