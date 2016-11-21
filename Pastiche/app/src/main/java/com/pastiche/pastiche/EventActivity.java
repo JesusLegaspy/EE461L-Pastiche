14300b775ce5c3f030a6163a39a85165271a5e91
ariapahlavan <apahlavan1@utexas.edu>
2016-11-20 19:55:02 -0600

    implement activity event without the use of a fragment + small tweaks in camera activity

--- Pastiche/app/src/main/java/com/pastiche/pastiche/EventActivity.java
+++ Pastiche/app/src/main/java/com/pastiche/pastiche/EventActivity.java
@@ -2,6 +2,9 @@ package com.pastiche.pastiche;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import com.pastiche.pastiche.adapter.PhotoListAdapter;
 
 /**
  * Created by Aria Pahlavan on 11/12/16.
@@ -14,5 +17,20 @@ public class EventActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rec_view_event_activity);
+        recyclerView.setHasFixedSize(true);  //TODO might want to remove
+
+        PhotoListAdapter adapter = new PhotoListAdapter(recyclerView.getContext());
+        adapter.setHasStableIds(true);
+        recyclerView.setAdapter(adapter);
+
+        //TODO might need to change to allow download as scrolling
+        recyclerView.setHasFixedSize(true);
+        // Set padding for Tiles
+        int tilePadding = getResources().getDimensionPixelSize(R.dimen.tile_padding);
+        recyclerView.setPadding(tilePadding, tilePadding, tilePadding, tilePadding);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(3, 1));
+//        return recyclerView;
+
     }
 }
