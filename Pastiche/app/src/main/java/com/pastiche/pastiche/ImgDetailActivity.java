809f5ee0140e0ac3ffcf756da9c96c870453c3e5
Khalid Qarryzada <khalid.qarryzada@utexas.edu>
2016-11-24 16:54:20 -0600

    Deleted deprecate/ and body of ImgDetailActivity.java to prevent gradle from throwing errors

--- Pastiche/app/src/main/java/com/pastiche/pastiche/ImgDetailActivity.java
+++ Pastiche/app/src/main/java/com/pastiche/pastiche/ImgDetailActivity.java
@@ -11,38 +11,4 @@ import android.widget.TextView;
 
 public class ImgDetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "position";
-
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event);
-        setSupportActionBar((Toolbar) findViewById(R.id.main_toolbar));
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // Set Collapsing Toolbar layout to the screen
-        CollapsingToolbarLayout collapsingToolbar =
-                (CollapsingToolbarLayout) findViewById(R.id.img_detail_collap_toolbar);
-        // Set title of Detail page
-         collapsingToolbar.setTitle(getString(R.string.item_title));
-
-        int postion = getIntent().getIntExtra(EXTRA_POSITION, 0);
-        Resources resources = getResources();
-        String[] places = resources.getStringArray(R.array.events);
-        collapsingToolbar.setTitle(places[postion % places.length]);
-
-        String[] placeDetails = resources.getStringArray(R.array.place_details);
-        TextView placeDetail = (TextView) findViewById(R.id.place_detail);
-        placeDetail.setText(placeDetails[postion % placeDetails.length]);
-
-        String[] placeLocations = resources.getStringArray(R.array.place_locations);
-        TextView placeLocation =  (TextView) findViewById(R.id.place_location);
-        placeLocation.setText(placeLocations[postion % placeLocations.length]);
-
-        TypedArray placePictures = resources.obtainTypedArray(R.array.events_picture);
-        ImageView placePicutre = (ImageView) findViewById(R.id.img_det_picture);
-        placePicutre.setImageDrawable(placePictures.getDrawable(postion % placePictures.length()));
-
-        placePictures.recycle();
-    }
 }
