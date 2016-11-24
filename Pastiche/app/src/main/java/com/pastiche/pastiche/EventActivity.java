c1aec0d4b6d94d5ff2283d586f07b677697df76c
Jesus Legaspy <jesus.legaspy@utexas.edu>
2016-11-24 00:03:49 -0600

    fixed google play service insertion

--- Pastiche/app/src/main/java/com/pastiche/pastiche/EventActivity.java
+++ Pastiche/app/src/main/java/com/pastiche/pastiche/EventActivity.java
@@ -9,10 +9,6 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.View;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.pastiche.pastiche.adapter.PhotoListAdapter;
 
 import java.util.Random;
@@ -26,12 +22,6 @@ public class EventActivity extends AppCompatActivity {
     protected final int INVALID_ID = -1;
     protected int eventID = -1;
     protected PhotoListAdapter adapter = null;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,9 +45,6 @@ public class EventActivity extends AppCompatActivity {
 
         adapter.setHasStableIds(true);
         recyclerView.setAdapter(adapter);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -106,39 +93,13 @@ public class EventActivity extends AppCompatActivity {
         adapter.refresh();
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Event Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
     @Override
     public void onStart() {
         super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
     }
 
     @Override
     public void onStop() {
         super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
 }
